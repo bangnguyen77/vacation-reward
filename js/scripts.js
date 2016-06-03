bb75b1440a18dcd7f6c03a4277b629a412b96784
bangnguyen77 <bangnv77@gmail.com>
2016-06-03 16:50:50 -0700

    final fix but still long way to get it done

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,38 @@
 $(document).ready(function() {
-  $("#survey form").submit(function(event) {
 
+  $("#survey form").submit(function(event) {
     var nameInput = $("input#name").val();
     // var showInput = $("select#show").val();
     // var actorInput = $("select#actor").val();
     // var foodInput = $("select#food").val();
     // var beverageInput = $("select#beverage").val();
     // var carInput = $("select#car").val();
-    $(".name").text(nameInput);
+    //
+    // var vacation1 = Yosemite National Park;
+    // var vacation2 = Bahamas;
+    // var vacation3 = Paris;
 
-    $("#letter").show();
-    $("#survey").hide();
+    // if (showInput === "The Americans" && actorInput === "Adam Sandler") {
+      $("#survey").hide();
+      $("#award").show();
+      $(".name").text(nameInput);
+      // $(".award").append(vacation1);
+    // } else if (showInput === "Broad City" && actorInput === "Julianne Moore") {
+    //   $("#survey").hide();
+    //   $("#award").show();
+    //   $(".name").text(nameInput);
+    //   $(".award").text(vacation2);
+    // } else if (showInput === "American Crime Story" && actorInput === "Robert Downey Jr.") {
+    //   $("#survey").hide();
+    //   $("#award").show();
+    //   $(".name").text(nameInput);
+    //   $(".award").text(vacation3);
+    // } else {
+    //   $("#survey").hide();
+    //   $("#award").show().text("You need to watch more advertising!");
+    // }
 
     event.preventDefault();
   });
+
 });
-
-
-// $(document).ready(function(){
-//
-//   $("#nameform form").submit(function(event){
-//     var nameInput = $("input#name").val();
-//     // var showInput = $("select#show").val();
-//     // var actorInput = $("select#actor").val();
-//     // var foodInput = $("select#food").val();
-//     // var beverageInput = $("select#beverage").val();
-//     // var carInput = $("select#car").val();
-//     //
-//     // var vacation1 = Yosemite National Park;
-//     // var vacation2 = Bahamas;
-//     // var vacation3 = Paris;
-//
-//     $(".name").text(nameInput);
-//
-//
-//     $("#letter").show();
-//     $("#nameform").hide();
-//     event.preventDefault();
-//   });
-//
-// });
