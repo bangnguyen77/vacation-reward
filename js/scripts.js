cf062974622ed38fbac82aa8e757c6df51112cf6
Bang Nguyen <bangnv77@gmail.com>
2016-06-09 23:07:12 -0700

    add codes to give functionality to the page

--- js/scripts.js
+++ js/scripts.js
@@ -2,35 +2,47 @@ $(document).ready(function() {
 
   $("#survey form").submit(function(event) {
     var nameInput = $("input#name").val();
-    // var showInput = $("select#show").val();
-    // var actorInput = $("select#actor").val();
-    // var foodInput = $("select#food").val();
-    // var beverageInput = $("select#beverage").val();
-    // var carInput = $("select#car").val();
-    //
-    // var vacation1 = Yosemite National Park;
-    // var vacation2 = Bahamas;
-    // var vacation3 = Paris;
+    var showInput = $("select#show").val();
+    var actorInput = $("select#actor").val();
+    var foodInput = $("select#food").val();
+    var beverageInput = $("select#beverage").val();
+    var carInput = $("select#car").val();
 
-    // if (showInput === "The Americans" && actorInput === "Adam Sandler") {
+    var vacation1 = Yosemite National Park;
+    var vacation2 = Bahamas;
+    var vacation3 = Paris;
+
+    if (showInput === "The Americans" && actorInput === "Adam Sandler") {
+      $("#yosemite").fadeIn();
+      $("#bahamas").fadeOut();
+      $("#paris").fadeOut();
+
       $("#survey").hide();
       $("#award").show();
-      $(".name").text(nameInput);
-      // $(".award").append(vacation1);
-    // } else if (showInput === "Broad City" && actorInput === "Julianne Moore") {
-    //   $("#survey").hide();
-    //   $("#award").show();
-    //   $(".name").text(nameInput);
-    //   $(".award").text(vacation2);
-    // } else if (showInput === "American Crime Story" && actorInput === "Robert Downey Jr.") {
-    //   $("#survey").hide();
-    //   $("#award").show();
-    //   $(".name").text(nameInput);
-    //   $(".award").text(vacation3);
-    // } else {
-    //   $("#survey").hide();
-    //   $("#award").show().text("You need to watch more advertising!");
-    // }
+      $(".name").text(nameInput).show();
+      $(".award").text(vacation1);
+      } else if (showInput === "Broad City" && actorInput === "Julianne Moore") {
+        $("#yosemite").fadeOut();
+        $("#bahamas").fadeIn();
+        $("#paris").fadeOut();
+
+        $("#survey").hide();
+        $("#award").show();
+        $(".name").text(nameInput).show();
+        $(".award").text(vacation2);
+        } else if (showInput === "American Crime Story" && actorInput === "Robert Downey Jr.") {
+          $("#yosemite").fadeOut();
+          $("#bahamas").fadeOut();
+          $("#paris").fadeIn();
+
+          $("#survey").hide();
+          $("#award").show();
+          $(".name").text(nameInput).show();
+          $(".award").text(vacation3);
+          } else {
+            $("#survey").hide();
+            $("#award").show().text("You need to watch more advertising!");
+            }
 
     event.preventDefault();
   });
